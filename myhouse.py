9281f5ff41e2f86d5546f044c7d5f2f66e4e8377
Jacheson32 <54958579+Jacheson32@users.noreply.github.com>
2019-11-06 11:49:50 -0800

    Add files via upload

---
+++ myhouse.py
@@ -0,0 +1,103 @@
+from turtle import *
+space = Screen()          # create a turtle screen (space)
+bob = Turtle()            # create a turtle named bob
+
+
+# Make a square
+bob.begin_fill()
+bob.speed(0)
+bob.pensize(5)			#bob change pen thickness to 5
+bob.color ("moccasin")	
+bob.penup()
+bob.right(180)			
+bob.forward(200)		
+bob.right(180)
+bob.pendown()
+for count in range(4):
+	bob.forward(400)          
+	bob.right(90)             
+()
+bob.end_fill()
+# Make a roof
+bob.begin_fill()
+bob.color ("red")
+bob.forward(450)          
+bob.right(-90)           
+bob.circle(250,180)           #
+bob.end_fill()
+#make a window
+
+bob.penup()				  
+bob.forward(150)		 
+bob.right(-90)			  
+bob.forward(100)
+bob.right(90)			
+bob.pendown()				
+bob.color ("blue")
+bob.begin_fill()
+bob.circle(50)	
+
+
+bob.end_fill()
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.penup()
+bob.forward(125)
+bob.pendown()
+
+#first mushroom white circle for roof
+bob.left(6)
+bob.penup()
+bob.forward(150)
+bob.right(90)
+bob.forward(175)
+bob.left(90)
+bob.color("white")
+bob.begin_fill()
+bob.pendown()
+bob.circle(50,365)
+bob.end_fill()
+
+#second mushroom circle on roof
+bob.penup()
+bob.left(90)
+bob.forward(300)
+bob.left(90)
+bob.forward(50)
+bob.pendown()
+bob.begin_fill()
+bob.circle(35)
+bob.end_fill()
+
+#window boards
+bob.penup()
+bob.forward(200)
+bob.right(97)
+bob.forward(10)
+bob.right(183)
+bob.color("brown")
+bob.pendown()
+bob.forward(100)
+bob.right(180)
+bob.forward(50)
+bob.right(90)
+bob.forward(57)
+bob.right(180)
+bob.forward(100)
+
+#door
+bob.penup()
+bob.left(90)
+bob.forward(200)
+bob.pendown()
+bob.left(90)
+bob.circle(75,180)
+bob.forward(200)
+bob.left(89)
+bob.forward(150)
+bob.left(90)
+bob.forward(205)
+bob.left(90)
+bob.forward(150)
+exitonclick()				#exit code on click
